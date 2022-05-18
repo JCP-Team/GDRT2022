a86ea068987e9a878962b1565643b6e0aa390687
GitM3 <u04603852@tuks.co.za>
2022-05-18 18:19:33 +0200

    main

--- KAIZEN-STATION/src/main.cpp
+++ KAIZEN-STATION/src/main.cpp
@@ -5,6 +5,10 @@
 #include "sensirion_common.h"
 #include "sgp30.h"
 #include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include "ThingSpeak.h"
+#include "DHT.h"
+#include "Multichannel_Gas_GMXXX.h"
 
 
 #define PINNUMBER     "0000"
@@ -18,20 +22,22 @@
 GSMClient client;
 GPRS gprs;
 GSM gsmAccess;
+GSMScanner scanNet;
 
 char server[] = "ie-gtfs.up.ac.za";
 char path[] = "/data/post.php";
 int port = 80; // port 80 is the default for HTTP
-String apiKeyValue = "000ZZ4ND";
+unsigned long myChannelNumber = 1698096;
+const char * myWriteAPIKey = "4A9Q3S8W03GXJ51S";
 
 String postData;
-String postVariable = "test=";
 
 //I2C ===========================================================
   
   #include <Wire.h>
   TCA9548A<TwoWire> TCA;
   #define WIRE Wire
+  LiquidCrystal_I2C lcd(0x27,16,4);
 
 // SEEED_HM330X ============================================================
   #ifdef  ARDUINO_SAMD_VARIANT_COMPLIANCE
@@ -72,7 +78,6 @@ HM330XErrorCode parse_result(uint8_t* data, uint16_t* out) {
     }
     for (int i = 1; i < 8; i++) {
         value = (uint16_t) data[i * 2] << 8 | data[i * 2 + 1];
-      //  print_result(str[i - 1], value);
       out[i-1] = value;
 
     }
@@ -84,13 +89,7 @@ HM330XErrorCode parse_result_value(uint8_t* data) {
     if (NULL == data) {
         return ERROR_PARAM;
     }
-    // for (int i = 0; i < 28; i++) {
-    //     SERIAL_OUTPUT.print(data[i], HEX);
-    //     SERIAL_OUTPUT.print("  ");
-    //     if ((0 == (i) % 5) || (0 == i)) {
-    //         SERIAL_OUTPUT.println("");
-    //     }
-    // }
+  
     uint8_t sum = 0;
     for (int i = 0; i < 28; i++) {
         sum += data[i];
@@ -102,12 +101,49 @@ HM330XErrorCode parse_result_value(uint8_t* data) {
     return NO_ERROR;
 }
 
-//==========
+//DHT20 ===========================================================
+ DHT dht(DHT20); 
 
+// SEN0219 =====================
+  #define  sensorIn  0   // Sensor PWM interface
+#define  interruptNumber  0   // interrupt number
+
+unsigned long pwm_high_start_ticks=0, pwm_high_end_ticks=0;
+float pwm_high_val=0, pwm_low_val=0;
+volatile uint8_t flag=0;
+
+void interrupt_falling();
+void interrupt_rising()
+{
+  pwm_high_start_ticks = micros();    // store the current micros() value
+  if(2 == flag){
+    flag = 4;
+    pwm_low_val = pwm_high_start_ticks - pwm_high_end_ticks;
+  }else{
+    flag = 1;
+  }
+
+  attachInterrupt(interruptNumber, interrupt_falling, FALLING);
+}
+
+void interrupt_falling()
+{
+  pwm_high_end_ticks = micros();    // store the current micros() value
+  if(1 == flag){
+    flag = 2;
+    pwm_high_val = pwm_high_end_ticks - pwm_high_start_ticks;
+  }
+
+  attachInterrupt(interruptNumber, interrupt_rising, RISING);
+}
+
+//===========MULTI GAS
+ 
+    GAS_GMXXX<TwoWire> multi_gas;
 //
 
 void setup() {
-
+ 
   SERIAL_OUTPUT .begin(BAUD_RATE);
   while (!SERIAL_OUTPUT ) {
     ; // wait for serial port to connect. Needed for native USB port only
@@ -115,15 +151,21 @@ void setup() {
   TCA.begin(WIRE);
   TCA.openChannel(TCA_CHANNEL_0);   
   TCA.openChannel(TCA_CHANNEL_1);
+  TCA.openChannel(TCA_CHANNEL_2);
+  TCA.openChannel(TCA_CHANNEL_3);
   /* SENSOR INITIATION:
 
   */
+ multi_gas.begin(Wire,0x08);
+ dht.begin();
  SERIAL_OUTPUT.println("STARTING");
-
   if (sensor_HM330X.init()) {
         SERIAL_OUTPUT.println("HM330X init failed!!");  
   }
   SERIAL_OUTPUT.println("HM330X init ok");
+
+pinMode(sensorIn, INPUT);
+attachInterrupt(interruptNumber, interrupt_rising, RISING);
 
 s16 err;
 u16 scaled_ethanol_signal, scaled_h2_signal;
@@ -143,6 +185,7 @@ u16 scaled_ethanol_signal, scaled_h2_signal;
     err = sgp_iaq_init();
 
 
+
   /* GSM INITIATION:
 
   */
@@ -156,18 +199,25 @@ u16 scaled_ethanol_signal, scaled_h2_signal;
     if ((gsmAccess.begin(PINNUMBER) == GSM_READY) &&
         (gprs.attachGPRS(GPRS_APN, GPRS_LOGIN, GPRS_PASSWORD) == GPRS_READY)) {
       connected = true;
+       Serial.println("Signal strength: " + scanNet.getSignalStrength());
+       ThingSpeak.begin(client);
+
     } else {
       SERIAL_OUTPUT .println("Not connected");
       delay(1000);
     }
   }
-
+  
   SERIAL_OUTPUT .println("connecting...");
+
+
 }
 
 void loop() {
-  
-
+  Serial.println("Signal strength: " + scanNet.getSignalStrength());
+   // signal strength in 0-31 scale. 31 means power > 51dBm
+  // BER is the Bit Error Rate. 0-7 scale. 99=not detectable
+ 
   /*
     Sensor Activation:
   */
@@ -177,68 +227,107 @@ void loop() {
   }
   parse_result_value(buf);
   parse_result(buf,HM330X_values);
-  
-  delay(5000);
+   float SENO_concentration =0;
+  if(flag == 4){
+    flag = 1;
+    float pwm_high_val_ms = (pwm_high_val * 1000) / (pwm_low_val + pwm_high_val);
+
+    if (pwm_high_val_ms < 0.01){
+      Serial.println("Fault");
+    }
+    else if (pwm_high_val_ms < 80.00){
+      Serial.println("preheat");
+    }
+    else if (pwm_high_val_ms < 998.00){
+       SENO_concentration = (pwm_high_val_ms - 2) * 5;
+      // Print pwm_high_val_ms
+      Serial.print("pwm_high_val_ms:");
+      Serial.print(pwm_high_val_ms);
+      Serial.println("ms");
+      //Print CO2 concentration
+      Serial.print(SENO_concentration);
+      Serial.println("ppm");
+    }
+    else{
+      Serial.println("Beyond the maximum range ：398~4980ppm");
+    }
+    Serial.println();
+  }
 
 
   s16 err = 0;
   u16 tvoc_ppb, co2_eq_ppm;
     err = sgp_measure_iaq_blocking_read(&tvoc_ppb, &co2_eq_ppm);
-    if (err == STATUS_OK) {
-        // Serial.print("tVOC  Concentration:");
-        // Serial.print(tvoc_ppb);
-        // Serial.println("ppb");
-
-        // Serial.print("CO2eq Concentration:");
-        // Serial.print(co2_eq_ppm);
-        // Serial.println("ppm");
-    } else {
+    if (err != STATUS_OK) {
         Serial.println("error reading IAQ values\n");
     }
+   float temp_hum_val[2] = {0};
+   if (!dht.readTempAndHumidity(temp_hum_val)) {
+   }else {Serial.println("DHT20 FAIL");}
+
+uint32_t val_NO2 = multi_gas.measure_NO2();
+uint32_t val_C2H50H = multi_gas.measure_C2H5OH();
+uint32_t val_VOC = multi_gas.measure_VOC();
+uint32_t val_CO = multi_gas.measure_CO();
+Serial.println("N02: " +String(val_NO2) + " C2H50H: " +String(val_C2H50H) + " VOC: " +String(val_VOC )+ " CO: " +String(val_CO));
   /*
     Data formating:
   */
-  // String httpDATA ="api_key=" + apiKeyValue;
+
+  // String httpDATA ="&api_key=" + apiKeyValue;
   // for(int i=0; i<8;i++)
   //     httpDATA += "&value" +String(i)+'='+String(HM330X_values[i]);
   // httpDATA += "&tvoc_ppb=" +String(tvoc_ppb) +"&co2_eq_ppm=" + String(co2_eq_ppm)+"";
   String httpDATA ="&str=";
-  String sign = " : ";
-  for(int i=0; i<8;i++){
-      httpDATA.concat(i);
-      httpDATA.concat(sign);
-      httpDATA+=HM330X_values[i];
-      httpDATA+=" ";
+  String sign = "  ";
+  for(int i=1; i<4;i++){
+      ThingSpeak.setField(i, HM330X_values[i]);
   }
-    httpDATA += "tvoc_ppb-->" +tvoc_ppb +String("co2_eq_ppm-->") + co2_eq_ppm+"";
+    ThingSpeak.setField(4,String(val_VOC)); //VOC
+    ThingSpeak.setField(5,co2_eq_ppm);
+  if(temp_hum_val[0]!=0) ThingSpeak.setField(6, temp_hum_val[0]);
+   if(temp_hum_val[1]!=0)ThingSpeak.setField(7, temp_hum_val[1]);
+  if(SENO_concentration!=0) ThingSpeak.setField(8,SENO_concentration);
+
   /*
     HTTP Activation:
   */
+   
     int res_connect = client.connect(server, 80);
     if (res_connect) {
       Serial.println("SERVER: OK");
-
-      client.print("POST ");
-      client.print(path);
-      client.println(" HTTP/1.1");
-      client.println("Host: ie-gtfs.up.ac.za");
-      client.println("Content-Type: application/x-www-form-urlencoded");
-      client.print("Content-Length: ");
-      client.println(httpDATA.length());
-      client.println();
-      client.print(httpDATA);
-      Serial.println(httpDATA);
+      int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+      if(x == 200){
+        Serial.println("Update successful.");
+      }
+      else{
+        Serial.println(" HTTP error: " + String(x));
+      }
+      
+      // client.print("POST ");
+      // client.print(path);
+      // client.println(" HTTP/1.1");
+      // client.println("Host: ie-gtfs.up.ac.za");
+      // client.println("Connection: close");
+      // client.println("Content-Type: application/x-www-form-urlencoded");
+      // client.print("Content-Length: ");
+      // client.println(httpDATA.length());
+      // client.println();
+      // client.print(httpDATA);
+     
+      //Serial.println(httpDATA);
+      
       //if (client.connected()) client.stop();
-      String response = "";
-        long time = millis();
-        while(millis() - time < 10000){
-          while (client.available() > 0) {
-            char he = client.read();
-            response += he; 
-          } 
-        }
-        Serial.println("Printing response: ");
-        Serial.println(response);
+      //  String response = "";
+      //   long time = millis();
+      //   while(millis() - time < 10000){
+      //     while (client.available() > 0) {
+      //       char he = client.read();
+      //       response += he; 
+      //     } 
+      //   }
+      //   Serial.println("Printing response: ");
+      //   Serial.println(response);
       }else {
       // if you didn't get a connection to the server
       Serial.println("SERVER: ERROR");
---
+++ NANO/KAIZEN-NANO/src/main.cpp
@@ -0,0 +1,213 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <Seeed_HM330X.h>
+#include "sensirion_common.h"
+#include <Wire.h>
+//#include "DHT.h"
+#include "SCD30.h"
+#include "Multichannel_Gas_GMXXX.h"
+#define SIM800_TX_PIN 2  //SIM800 TX is connected to Arduino D2  
+#define SIM800_RX_PIN 3 //SIM800 RX is connected to Arduino D3
+//I2C A4 SDA -- A5 SCL
+#define battPin A0
+//int PWX=4; //pull up pin for SIM800
+
+
+  #ifdef  ARDUINO_SAMD_VARIANT_COMPLIANCE
+    #define SERIAL_OUTPUT SerialUSB
+#else
+    #define SERIAL_OUTPUT Serial
+#endif
+ 
+// Communication ==========================================================================
+SoftwareSerial serialSIM800(SIM800_TX_PIN,SIM800_RX_PIN);
+  
+char server[] = "http://ie-gtfs.up.ac.za";
+char path[] = "/data/z-nano.php";
+int port = 80; // port 80 is the default for HTTP
+String postData;
+
+// Sensors =======================================================
+  HM330X sensor_HM330X;
+  uint8_t buf[30];
+  uint16_t HM330X_values[8];
+  /*parse buf with 29 uint8_t-data*/
+  HM330XErrorCode parse_result(uint8_t* data, uint16_t* out)
+   {
+    uint16_t value = 0;
+    if (NULL == data) {
+        return ERROR_PARAM;
+    }
+    for (int i = 1; i < 8; i++) {
+        value = (uint16_t) data[i * 2] << 8 | data[i * 2 + 1];
+      out[i-1] = value;
+
+    }
+    return NO_ERROR;
+}
+
+//DHT20 ===========================================================
+// DHT dht(DHT20); 
+
+//===========MULTI GAS
+ 
+    GAS_GMXXX<TwoWire> multi_gas;
+//
+String _readSerial(){
+  
+  int timeout=0;
+  while (serialSIM800.available() != 0 && timeout< 10000)
+  {
+    Serial.write(serialSIM800.read());
+    delay(10);
+    timeout++;
+  }
+  Serial.flush();
+  if (serialSIM800.available()) {
+ 	return serialSIM800.readString();
+  }
+  
+
+}
+ 
+void setup() {
+  scd30.initialize();  
+  pinMode(PWX, OUTPUT);
+ 
+  Serial.begin(9600);   
+  while(!Serial);    
+  serialSIM800.begin(9600);
+  delay(1000);
+
+  // serialSIM800.print(F("AT\r\n"));
+  // while (_readSerial().indexOf("OK")==-1 ){
+  //   serialSIM800.print(F("AT\r\n"));
+  // }
+   
+  Serial.println(F("Setup Complete!"));
+ 
+ serialSIM800.print("AT+SAPBR=3,1,\"Contype\",\"GPRS\"");
+delay(2000);
+
+serialSIM800.print("AT+SAPBR=3,1,\"APN\",\"afrihost\"");
+delay(2000);
+serialSIM800.print("AT+SAPBR=1,1");
+delay(2000);
+serialSIM800.print("AT+SAPBR=2,1");
+delay(2000);
+
+  
+  /* SENSOR INITIATION:
+
+  */
+
+ //dht.begin();
+ multi_gas.begin(Wire,0x08);
+ sensor_HM330X.init();
+
+}
+ 
+void loop() {
+  // //Read SIM800 output (if available) and print it in Arduino IDE Serial Monitor
+  // if(serialSIM800.available()){
+  //   Serial.write(serialSIM800.read());
+  // }
+  // //Read Arduino IDE Serial Monitor inputs (if available) and send them to SIM800
+  // if(Serial.available()){    
+  //   serialSIM800.write(Serial.read());
+  // }
+int val = 0;
+val = analogRead(battPin);  // read the input pin
+float batt_m = 4850 * float(val)/1024;
+
+sensor_HM330X.read_sensor_value(buf, 29) ;
+ parse_result(buf,HM330X_values);
+
+// float SENO_conc=0;
+// float voltage =  analogRead(SEN019)*(5000/1024.0);
+// Serial.println("Volts" + String(voltage));
+//  if(voltage == 0)
+//  {
+//  Serial.println("Fault");
+//  }
+//  else if(voltage < 400)
+//  { //pre-heating
+//  } else SENO_conc=(voltage-400)*50.0/16.0; 
+ 
+//  float SENO_concentration =0;
+//   if(flag == 4){
+//     flag = 1;
+//     float pwm_high_val_ms = (pwm_high_val * 1000) / (pwm_low_val + pwm_high_val);
+
+//     if (pwm_high_val_ms < 0.01){
+//       Serial.println("Fault");
+//     }
+//     else if (pwm_high_val_ms < 80.00){
+//       Serial.println("preheat");
+//     }
+//     else if (pwm_high_val_ms < 998.00){
+//        SENO_concentration = (pwm_high_val_ms - 2) * 5;
+//       // Print pwm_high_val_ms
+//       Serial.print("pwm_high_val_ms:");
+//       Serial.print(pwm_high_val_ms);
+//       Serial.println("ms");
+//       //Print CO2 concentration
+//       Serial.print(SENO_concentration);
+//       Serial.println("ppm");
+//     }
+//     else{
+//       Serial.println("Beyond the maximum range ：398~4980ppm");
+//     }
+//     Serial.println();
+//   }
+
+
+float result[3] = {0}; //0: co2, 1: temp, 2: hum
+scd30.getCarbonDioxideConcentration(result);
+
+uint32_t val_NO2 = multi_gas.measure_NO2();
+uint32_t val_C2H50H = multi_gas.measure_C2H5OH();
+uint32_t val_VOC = multi_gas.measure_VOC();
+uint32_t val_CO = multi_gas.measure_CO();
+Serial.println("N02: " +String(val_NO2) + " C2H50H: " +String(val_C2H50H) + " VOC: " +String(val_VOC )+ " CO: " +String(val_CO));
+for(int i=1; i<3;i++)
+ Serial.println(" value" +String(i)+'='+String(HM330X_values[i]));
+
+Serial.println("CO2: " + String(result[0]) +" " + result[1] + " " + result[2]);
+// Serial.println("SENO: " + String(SENO_conc));
+delay(1000);
+
+postData= "?";
+postData+= "Sensor_ID=SEN_0" ;
+postData+= "&PM2=" +String(HM330X_values[1]);
+postData+= "&PM10="+String(HM330X_values[2]);
+postData+= "&VOC="+String(val_VOC);
+postData+= "&C0=" +String(val_CO);
+postData+= "&C02=" +String(result[0]);
+postData+= "&Temperature="+String(result[1]);
+// postData+= "&str=" + 2;
+// postData+= ":" +String(HM330X_values[1]);
+// postData+= ":"+String(HM330X_values[2]);
+// postData+= ":"+String(val_VOC);
+// postData+= ":" +String(val_CO);
+// postData+= ":" +String(result[0]);
+// postData+= ":"+String(result[1]);
+//postData = "?FROMARDUINO=100\"";
+
+serialSIM800.print("AT+HTTPINIT");
+delay(2000);
+serialSIM800.print( "AT+HTTPPARA=\"CID\",1");
+delay(2000);
+String url = "AT+HTTPPARA=\"URL\",\""+String(server)+path + String(postData) +"\"";
+Serial.println(url);
+serialSIM800.print(url); //url.c_str()
+delay(2000);
+serialSIM800.print("AT+HTTPPARA=\"PROPORT\",\"80\"");
+delay(2000);
+serialSIM800.print("AT+HTTPACTION=0");
+delay(10000);
+// _readSerial();
+serialSIM800.print("AT+HTTPTERM");
+                
+}
+ 
