0dad8cc0a100f65d9891ff7165e194798c0b853b
GitM3 <u04603852@tuks.co.za>
2022-05-30 10:32:50 +0200

    Library: edited init and start: error with initialisation I think

--- NANO/KAIZEN-NANO/src/main.cpp
+++ NANO/KAIZEN-NANO/src/main.cpp
@@ -2,11 +2,11 @@
 #include <SoftwareSerial.h>
 #include <Seeed_HM330X.h>
 #include "sensirion_common.h"
+#include "SIM800C.h"
 #include <Wire.h>
-//#include "DHT.h"
 #include "SCD30.h"
 #include "Multichannel_Gas_GMXXX.h"
-#include "BareBoneSim800.h"
+
 #define SIM800_TX_PIN 2  //SIM800 TX is connected to Arduino D2  
 #define SIM800_RX_PIN 3 //SIM800 RX is connected to Arduino D3
 //I2C A4 SDA -- A5 SCL
@@ -55,25 +55,23 @@ String postData;
     GAS_GMXXX<TwoWire> multi_gas;
 //
 
+SIM800C* sim;
  
 void setup() {
+  sim = new SIM800C(SIM800_TX_PIN,SIM800_RX_PIN);
+  sim->start();
+  Serial.begin(9600);
   scd30.initialize();  
-  pinMode(PWX, OUTPUT);
  
-  Serial.begin(9600);   
   while(!Serial);    
-  //serialSIM800.begin(9600);
-  serialSIM800.begin();
-   delay(8000);
-  
- serialSIM800.print("AT+SAPBR=3,1,\"Contype\",\"GPRS\"");
-`checkResponse()
-serialSIM800.print("AT+SAPBR=3,1,\"APN\",\"afrihost\"");
-delay(2000);
-serialSIM800.print("AT+SAPBR=1,1");
-delay(2000);
-serialSIM800.print("AT+SAPBR=2,1");
-delay(2000);
+  sim->apn("afrihost");
+  if(sim->init()) Serial.println("SIM OK");
+  int i = 10;
+  while(i>0 && !sim->http_init()){
+	  i--;
+	  delay(1000);
+    Serial.println("busy");
+  }
 
   
   /* SENSOR INITIATION:
@@ -81,26 +79,20 @@ delay(2000);
   */
 
  //dht.begin();
- multi_gas.begin(Wire,0x08);
- sensor_HM330X.init();
+//  multi_gas.begin(Wire,0x08);
+//  sensor_HM330X.init();
 
 }
+int i =0;
  
 void loop() {
-  // //Read SIM800 output (if available) and print it in Arduino IDE Serial Monitor
-  // if(serialSIM800.available()){
-  //   Serial.write(serialSIM800.read());
-  // }
-  // //Read Arduino IDE Serial Monitor inputs (if available) and send them to SIM800
-  // if(Serial.available()){    
-  //   serialSIM800.write(Serial.read());
-  // }
-int val = 0;
-val = analogRead(battPin);  // read the input pin
-float batt_m = 4850 * float(val)/1024;
 
-sensor_HM330X.read_sensor_value(buf, 29) ;
- parse_result(buf,HM330X_values);
+// int val = 0;
+// val = analogRead(battPin);  // read the input pin
+// float batt_m = 4850 * float(val)/1024;
+
+// sensor_HM330X.read_sensor_value(buf, 29) ;
+//  parse_result(buf,HM330X_values);
 
 // float SENO_conc=0;
 // float voltage =  analogRead(SEN019)*(5000/1024.0);
@@ -141,29 +133,29 @@ sensor_HM330X.read_sensor_value(buf, 29) ;
 //   }
 
 
-float result[3] = {0}; //0: co2, 1: temp, 2: hum
-scd30.getCarbonDioxideConcentration(result);
+// float result[3] = {0}; //0: co2, 1: temp, 2: hum
+// scd30.getCarbonDioxideConcentration(result);
 
-uint32_t val_NO2 = multi_gas.measure_NO2();
-uint32_t val_C2H50H = multi_gas.measure_C2H5OH();
-uint32_t val_VOC = multi_gas.measure_VOC();
-uint32_t val_CO = multi_gas.measure_CO();
-Serial.println("N02: " +String(val_NO2) + " C2H50H: " +String(val_C2H50H) + " VOC: " +String(val_VOC )+ " CO: " +String(val_CO));
-for(int i=1; i<3;i++)
- Serial.println(" value" +String(i)+'='+String(HM330X_values[i]));
+// uint32_t val_NO2 = multi_gas.measure_NO2();
+// uint32_t val_C2H50H = multi_gas.measure_C2H5OH();
+// uint32_t val_VOC = multi_gas.measure_VOC();
+// uint32_t val_CO = multi_gas.measure_CO();
+// Serial.println("N02: " +String(val_NO2) + " C2H50H: " +String(val_C2H50H) + " VOC: " +String(val_VOC )+ " CO: " +String(val_CO));
+// for(int i=1; i<3;i++)
+//  Serial.println(" value" +String(i)+'='+String(HM330X_values[i]));
 
-Serial.println("CO2: " + String(result[0]) +" " + result[1] + " " + result[2]);
-// Serial.println("SENO: " + String(SENO_conc));
-delay(1000);
+// Serial.println("CO2: " + String(result[0]) +" " + result[1] + " " + result[2]);
+// // Serial.println("SENO: " + String(SENO_conc));
+// delay(1000);
 
-postData= "?";
-postData+= "Sensor_ID=SEN_0" ;
-postData+= "&PM2=" +String(HM330X_values[1]);
-postData+= "&PM10="+String(HM330X_values[2]);
-postData+= "&VOC="+String(val_VOC);
-postData+= "&C0=" +String(val_CO);
-postData+= "&C02=" +String(result[0]);
-postData+= "&Temperature="+String(result[1]);
+// postData= "?";
+// postData+= "Sensor_ID=SEN_0" ;
+// postData+= "&PM2=" +String(HM330X_values[1]);
+// postData+= "&PM10="+String(HM330X_values[2]);
+// postData+= "&VOC="+String(val_VOC);
+// postData+= "&C0=" +String(val_CO);
+// postData+= "&C02=" +String(result[0]);
+// postData+= "&Temperature="+String(result[1]);
 // postData+= "&str=" + 2;
 // postData+= ":" +String(HM330X_values[1]);
 // postData+= ":"+String(HM330X_values[2]);
@@ -171,23 +163,14 @@ postData+= "&Temperature="+String(result[1]);
 // postData+= ":" +String(val_CO);
 // postData+= ":" +String(result[0]);
 // postData+= ":"+String(result[1]);
-//postData = "?FROMARDUINO=100\"";
+postData = "?FROMARDUINO=100\"";
 
-// serialSIM800.print("AT+HTTPINIT");
-// serialSIM800.print( "AT+HTTPPARA=\"CID\",1");
-
-//String url = "AT+HTTPPARA=\"URL\",\""+String(server)+path + String(postData) +"\"";
-String url = String(server)+path + String(postData);
  Serial.println("Making HTTP Get Request");
-  String response = serialSIM800.sendHTTPData(url.c_str());
+  String response = sim->http_send(postData);
     Serial.println("Received Info: ");
     Serial.println(response);
-// serialSIM800.print(url); //url.c_str()
-// serialSIM800.print("AT+HTTPPARA=\"PROPORT\",\"80\"");
-// serialSIM800.print("AT+HTTPACTION=0");
-
-// _readSerial();
-
                 
+   delay(5000);             
+   if( i++ == 5) sim->debug();
 }
  
